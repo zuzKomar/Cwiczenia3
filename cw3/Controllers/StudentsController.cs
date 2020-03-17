7baf393637a06540a184bdbed1ce630bfc5f736b
zuzannas17301 <s17301@pjwstk.edu.pl>
2020-03-17 23:34:46 +0100

    do dokończenia

---
+++ cw3/Controllers/StudentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{
+    [Route("api/students")]      //jesli otrzyma jakies żądanie http na tą sciezke, to bedzie przekierowany do tego kontrolera
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet] //tylko rządania http get beda realizowane przez ta metode
+        
+        public string GetStudents()
+        {
+            return "Anna, Krzysztof i Basia";
+        }
+    }
+}
