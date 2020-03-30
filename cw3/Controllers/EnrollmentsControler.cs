f784fb138ec3e1e37330963450cd480ccda8ed88
zuzannas17301 <s17301@pjwstk.edu.pl>
2020-03-30 17:03:25 +0200

    Cw5

---
+++ cw3/Controllers/EnrollmentsControler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.SqlClient;
+using cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{
+    [Route("api/enrollments")]
+    [ApiController]
+
+    public class EnrollmentsControler : ControllerBase
+    {
+        private const string ConString = "Data Source=db-mssql;Initial Catalog=s17301;Integrated Security=True";
+
+        [HttpPost]
+        public IActionResult createEnrollment(Student student)
+        {
+            using (var con = new SqlConnection(ConString)) //polacznie do bazy
+            using (var com = new SqlCommand()) //zapytanie SQL wysylane do serwera
+            {
+                com.Connection = con;
+                com.CommandText = "";
+
+
+                    con.Open(); //otwarcie połączenia do bazy danych
+                var dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    var st = new Student();
+                    if (student.IndexNumber.Contains(null))
+                    {
+                        return NotFound();
+                    }
+                    st.IndexNumber = student.IndexNumber;
+                    st.FirstName = student.FirstName;
+                    st.LastName = student.LastName;
+                    st.BirthDate = student.BirthDate;
+                    st.StudiesName = student.StudiesName;
+                    return Ok(st);
+                }
+            }
+
+            return NotFound();
+        }
+    
+
+}
